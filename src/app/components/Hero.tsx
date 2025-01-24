e8dd101829ba3e39c2fb2de4923d31b6294d06d5
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-24 09:38:41 +0100

    add image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,14 +2,12 @@
 import Image from "next/image";
 import { ColourfulText } from "./ui/colourful-text";
 import Link from "next/link";
-import HeroImage from "../../../public/Unsplash.png";
-
+import HeroImage from "../../../public/Unsplash.webp";
 
 export function Hero() {
   return (
     <>
       <div className="flex flex-col poppins xl:flex-row items-center justify-between w-full max-w-screen-xl 2xl:max-w-screen-2xl m-auto px-8 py-20">
-        {/* Left Section */}
         <div className="text-center xl:text-left space-y-6 max-w-lg">
           <h1 className="text-4xl font-bold poppins text-teal-950 ">
             Gérez la bibliothèque avec l&apos;application{" "}
@@ -34,7 +32,6 @@ export function Hero() {
           </div>
         </div>
 
-        {/* Right Section */}
         <div className="flex-shrink-0 py-10">
           <Image src={HeroImage} alt="Hero" width={500} height={500} />
         </div>
